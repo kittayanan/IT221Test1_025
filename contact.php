9751b4801940c125e12c5d366c3ed4737b557807
kittayanan <148143363+kittayanan@users.noreply.github.com>
2026-02-20 15:44:57 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,25 +1,41 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+  <meta charset="utf-8">
+  <title>พรชัยศรีอพาตเเมนจำกัด</title>
+</head>
+
 <body bgcolor="#CC9900">
-    <table width="1238" height="530" border="0">
-        <tr>
-            <td width="23" rowspan="2">&nbsp;</td>
-            <td height="119" colspan="2" bgcolor="#006600">
-                <h1><center><font color="#FFFFFF">พรชัยศรีอพาตเเมนจำกัด</font></center></h1>
-                <?php include "Date.php"; ?>
-            </td>
-            <td width="43" rowspan="2">&nbsp;</td>
-        </tr>
-        <tr>
-            <td width="1007" bgcolor="#FFFFFF">
-                <img src="images/map.gif">
-            </td>
-            <td width="190" bgcolor="#FFFF66"><ul>
-				<li><a href="home.php">หน้าแรก</li>
-				<li><a href="room.php">ห้องพัก&ราคา</li>
-				<li><a herf="guest.php">สมุดเยี่ยม</li>
-				<li><a href="contact.php">ติดต่อ</li>
-				</ul><p>
-				<?php include "login_form.php";?>
-			</td>
-        </tr>
-    </table>
+  <table width="1238" height="530" border="0">
+    <tr>
+      <td width="23" rowspan="2">&nbsp;</td>
+
+      <td height="119" colspan="2" bgcolor="#006600" align="center">
+        <h1 style="color:#FFFFFF; margin:0;">พรชัยศรีอพาตเเมนจำกัด</h1>
+        <?php include "Date.php"; ?>
+      </td>
+
+      <td width="43" rowspan="2">&nbsp;</td>
+    </tr>
+
+    <tr>
+      <td width="1007" bgcolor="#FFFFFF">
+        <img src="images/map.gif" alt="แผนที่">
+      </td>
+
+      <td width="190" bgcolor="#FFFF66" valign="top">
+        <ul>
+          <li><a href="home.php">หน้าแรก</a></li>
+          <li><a href="room.php">ห้องพัก&amp;ราคา</a></li>
+          <li><a href="guest.php">สมุดเยี่ยม</a></li>
+          <li><a href="contact.php">ติดต่อ</a></li>
+        </ul>
+
+        <p>
+          <?php include "login_form.php"; ?>
+        </p>
+      </td>
+    </tr>
+  </table>
 </body>
+</html>
