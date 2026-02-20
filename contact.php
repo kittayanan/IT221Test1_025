618158b0a48c9c61b02d0478aac5272529bb39a4
kittayanan <148143363+kittayanan@users.noreply.github.com>
2026-02-20 16:14:12 +0700

    Fix title spelling in contact.php

--- contact.php
+++ contact.php
@@ -1,41 +1,41 @@
 <!DOCTYPE html>
 <html lang="th">
 <head>
-  <meta charset="utf-8">
-  <title>พรชัยศรีอพาตเเมนจำกัด</title>
+    <meta charset="utf-8">
+    <title>พรชัยศรีอพาตเมนต์จำกัด</title>
 </head>
 
 <body bgcolor="#CC9900">
-  <table width="1238" height="530" border="0">
-    <tr>
-      <td width="23" rowspan="2">&nbsp;</td>
+    <table width="1238" height="530" border="0">
+        <tr>
+            <td width="23" rowspan="2">&nbsp;</td>
 
-      <td height="119" colspan="2" bgcolor="#006600" align="center">
-        <h1 style="color:#FFFFFF; margin:0;">พรชัยศรีอพาตเเมนจำกัด</h1>
-        <?php include "Date.php"; ?>
-      </td>
+            <td height="119" colspan="2" bgcolor="#006600" align="center">
+                <h1 style="color:#FFFFFF; margin:0;">
+                    พรชัยศรีอพาตเมนต์จำกัด
+                </h1>
+                <?php include "Date.php"; ?>
+            </td>
 
-      <td width="43" rowspan="2">&nbsp;</td>
-    </tr>
+            <td width="43" rowspan="2">&nbsp;</td>
+        </tr>
 
-    <tr>
-      <td width="1007" bgcolor="#FFFFFF">
-        <img src="images/map.gif" alt="แผนที่">
-      </td>
+        <tr>
+            <td width="1007" bgcolor="#FFFFFF">
+                <img src="images/map.gif" alt="แผนที่">
+            </td>
 
-      <td width="190" bgcolor="#FFFF66" valign="top">
-        <ul>
-          <li><a href="home.php">หน้าแรก</a></li>
-          <li><a href="room.php">ห้องพัก&amp;ราคา</a></li>
-          <li><a href="guest.php">สมุดเยี่ยม</a></li>
-          <li><a href="contact.php">ติดต่อ</a></li>
-        </ul>
+            <td width="190" bgcolor="#FFFF66" valign="top">
+                <ul>
+                    <li><a href="home.php">หน้าแรก</a></li>
+                    <li><a href="room.php">ห้องพัก&amp;ราคา</a></li>
+                    <li><a href="guest.php">สมุดเยี่ยม</a></li>
+                    <li><a href="contact.php">ติดต่อ</a></li>
+                </ul>
 
-        <p>
-          <?php include "login_form.php"; ?>
-        </p>
-      </td>
-    </tr>
-  </table>
+                <?php include "login_form.php"; ?>
+            </td>
+        </tr>
+    </table>
 </body>
 </html>
